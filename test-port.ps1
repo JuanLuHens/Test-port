840d7e1a7ef7e7d77c73ffa617a088510d746772
Juan Lu Hens <juanlu82@gmail.com>
2022-05-25 20:22:09 +0200

    Init 0.1 script simple

---
+++ test-port.ps1
@@ -0,0 +1,18 @@
+﻿$ips = Get-Content ./ips.txt
+$ports = (1..1000)
+
+
+foreach (${ip} in ${ips}) {
+    $resultado = @{}
+    foreach (${port} in ${ports}) {
+        if (Test-Connection $ip -TcpPort $port) {
+            #Write-Host($ip+":"+$port) -ForegroundColor Green
+            $resultado[$port] = "Abierto"
+        }else{
+            #Write-Host($ip+":"+$port) -ForegroundColor Red
+            $resultado[$port] = "Cerrado"
+
+        }
+    }
+    $resultado.GetEnumerator() | Select-Object -Property @{N='Puerto';E={$_.Key}},@{N='Status';E={$_.Value}} | Export-Csv -Path /tmp/$ip.csv -Append -Encoding ASCII -NoTypeInformation -Delimiter ";" -Force 
+}
